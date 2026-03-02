01c75dc1261aea0d8155893e97c97be6a1d8b18d
beolkkul <melissamandinga@gmail.com>
2026-03-02 15:11:38 +0000

    first commit

---
+++ src/components/searchSection.jsx
@@ -0,0 +1,41 @@
+const SearchSection = ({ getWeatherDetails, searchInputRef }) => {
+
+    const API_KEY = import.meta.env.VITE_API_KEY;
+
+    // Function to fetch weather details based on the API URL
+    const handleCitySearch = (e) => {
+      e.preventDefault();
+      const searchInput = e.target.querySelector(".search-input");
+      console.log("Form submitted. City:", searchInput.value); // Log the searched city
+      const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${searchInput.value}&days=2`;
+      getWeatherDetails(API_URL); // Call the function to fetch weather details
+    }
+// gets user's current location (latitude/longitude)
+const handleLocationSearch = () => {
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const {latitude, longitude} = position.coords;
+            const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${latitude},${longitude}&days=2`;
+             getWeatherDetails(API_URL); // fetches weather details for the user's current location
+             window.innerWidth >= 768 && searchInputRef.current.focus();
+        },
+        () => {
+            alert("Location access denied. Please enable permissions to use this feature.");
+        }
+    )
+}
+
+    return (
+          <div className="search-section">
+      <form action="#" className="search-form" onSubmit={handleCitySearch}>
+        <span className="material-symbols-rounded">location_searching</span>
+        <input type="text" placeholder="Enter a city name" ref={searchInputRef} className="search-input" required />
+      </form>
+      <button className="location-button" onClick={handleLocationSearch}>
+        <span className="material-symbols-rounded">location_searching</span>
+      </button>
+    </div>
+    );
+};
+
+export default SearchSection;
